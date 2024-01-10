1ffe2d2246dbe4b1d5ce9f4aabc56c3e87e54974
Sławomir Gawryluk <slawomir.gawryluk@gmail.com>
2024-01-10 12:00:23 +0100

    aaa

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-let a = 5;
+let a = 6;
 
 root.render(
   <React.StrictMode>
