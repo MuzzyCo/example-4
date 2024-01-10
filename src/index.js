02e454f652049df54148718c02917a27d8fd01cb
Sławomir Gawryluk <slawomir.gawryluk@gmail.com>
2024-01-10 11:59:08 +0100

    pierwszy commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-let a = 3;
+let a = 5;
 
 root.render(
   <React.StrictMode>
